df775ef40d3dc7e5a8a914fc030287b7a35cc138
Benjamin <bpena@udesa.edu.ar>
2022-03-16 08:18:55 -0300

    Trabajo Autos

---
+++ db/index.js
@@ -0,0 +1,55 @@
+let autos = {
+    lista : [
+        {marca = "VW",
+        modelo = "Golf R",
+        año = 2019,
+        color = "Moss Green"
+        } , 
+        {marca = "Toyota",
+        modelo = "GR86",
+        año = 2022,
+        color = "Red"
+        } , 
+        {marca = "Land Rover",
+        modelo = "Defender",
+        año = 1998,
+        color = "Gray"
+        } ,
+        {marca = "BMW",
+        modelo = "135i Coupe",
+        año = 2011,
+        color = "Alpine White"
+        } , 
+        {marca = "Mercedes Benz",
+        modelo = "A35 AMG",
+        año = 2021,
+        color = "Sun Yellow"
+        } , 
+        {marca = "BMW",
+        modelo = "E30 M3",
+        año = 1986,
+        color = "Red"
+        } , 
+        {marca = "Tesla",
+        modelo = "S Plaid",
+        año = 2021,
+        color = "Silver Metallic"
+        } , 
+        {marca = "VW",
+        modelo = "Amarok V6 Extreme",
+        año = 2021,
+        color = "Black"
+        } , 
+        {marca = "Mercedes Benz",
+        modelo = "c63 amg",
+        año = 2015,
+        color = "Gray"
+        } , 
+        {marca = "Ford",
+        modelo = "Mustang GT500",
+        año = 1967,
+        color = "Gray"
+        },
+    ]
+}
+module.exports = autos;

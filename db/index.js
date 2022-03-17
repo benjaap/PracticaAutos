02a05e306adf00fe9a4fc804c5c08da915b7f90a
Benjamin <bpena@udesa.edu.ar>
2022-03-16 23:32:38 -0300

    Correcciones

--- db/index.js
+++ db/index.js
@@ -1,51 +1,61 @@
-let autos = {
-    lista : [
-        {marca = "VW",
+const autos = {
+    lista: [
+        {
+        marca = "VW",
         modelo = "Golf R",
         año = 2019,
-        color = "Moss Green"
+        color = "Green"
         } , 
-        {marca = "Toyota",
+        {
+        marca = "Toyota",
         modelo = "GR86",
         año = 2022,
         color = "Red"
         } , 
-        {marca = "Land Rover",
+        {
+        marca = "Land Rover",
         modelo = "Defender",
         año = 1998,
         color = "Gray"
         } ,
-        {marca = "BMW",
+        {
+        marca = "BMW",
         modelo = "135i Coupe",
         año = 2011,
-        color = "Alpine White"
+        color = "White"
         } , 
-        {marca = "Mercedes Benz",
+        {
+        marca = "Mercedes Benz",
         modelo = "A35 AMG",
         año = 2021,
-        color = "Sun Yellow"
+        color = "Yellow"
         } , 
-        {marca = "BMW",
+        {
+        marca = "BMW",
         modelo = "E30 M3",
         año = 1986,
         color = "Red"
         } , 
-        {marca = "Tesla",
+        {
+        marca = "Tesla",
         modelo = "S Plaid",
         año = 2021,
-        color = "Silver Metallic"
+        color = "Black"
         } , 
-        {marca = "VW",
+        {
+        marca = "VW",
         modelo = "Amarok V6 Extreme",
         año = 2021,
         color = "Black"
         } , 
-        {marca = "Mercedes Benz",
+        {
+        marca = "Mercedes Benz",
         modelo = "c63 amg",
         año = 2015,
         color = "Gray"
         } , 
-        {marca = "Ford",
+        {
+        marca = "Ford",
         modelo = "Mustang GT500",
         año = 1967,
         color = "Gray"
